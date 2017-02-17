3e9348a5f12c2b2ef8a0d7a6d4b253f742e43ddf
SamMcRuvie <doctorwhat35@gmail.com>
2017-02-17 15:28:48 +0000

    added all files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var x = function(a,b,c){
+c=new XMLHttpRequest;
+c.open('GET',a);
+c.onload=b;
+c.send()},
+yql=function(a,b){
+  return 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent('select * from '+b+' where url=\"'+a+'\"')+'&format=json';
+};
+
+x(yql('http://feeds.bbci.co.uk/news/rss.xml','xml'),function(){
+
+  var res = JSON.parse(this.response);
+  var channel = res.query.results.rss.channel;
+  console.log(channel);
+
+})
