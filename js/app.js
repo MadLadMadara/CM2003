b139675bfad855e4133d9c2fcace182e5721c07f
SamMcRuvie <doctorwhat35@gmail.com>
2017-02-24 15:57:02 +0000

    updated app.js with new rss feed reader method

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
-$(document).foundation(); 
-var x = function(a,b,c){
-c=new XMLHttpRequest;
-c.open('GET',a);
-c.onload=b;
-c.send()},
-yql=function(a,b){
+$(document).foundation();
+var bbc;
+
+
+
+// var x = function(a,b,c){
+// c=new XMLHttpRequest;
+// c.open('GET',a);
+// c.onload=b;
+// c.send()
+// };
+
+var yql=function(a,b){
   return 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent('select * from '+b+' where url=\"'+a+'\"')+'&format=json';
 };
 
-x(yql('http://feeds.bbci.co.uk/news/rss.xml','xml'),function(){
+  // x(yql('http://feeds.bbci.co.uk/news/rss.xml','xml'),function()
+  // {
+  //
+  // var res = JSON.parse(this.response);
+  // var channel = res.query.results.rss.channel;
+  // console.log(channel);
+  // bbc = channel.copyright;
+  // console.log(bbc);
+  //
+  // }
 
-  var res = JSON.parse(this.response);
-  var channel = res.query.results.rss.channel;
-  console.log(channel);
+  var yql = yql('http://feeds.bbci.co.uk/news/rss.xml','xml');
 
-})
+   $.getJSON(yql, function(res) {
+      var channel = res.query.results.rss.channel;
+       console.log(res);
+       console.log(channel);
+
+   }, "jsonp");
